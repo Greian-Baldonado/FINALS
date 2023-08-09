96939360d985e187eb6a227370bd3cb6e2179d0f
KIF222 <141860276+KIF222@users.noreply.github.com>
2023-08-10 02:48:20 +0800

    Add files via upload

--- ACT 8/main.py
+++ ACT 8/main.py
@@ -17,16 +17,15 @@ def face_confidence(face_distance, face_match_threshold = 0.6):
         return str(round(value, 2)) + '%'
     
 
-class Facerecognition:
+class FaceRecognition:
     face_locations = []
-    face_encodings = []
+    face_encoding = []
     face_names = []
     known_face_names = []
     process_current_frame = True
 
     def __init__(self):
-        pass 
-        #encode faces
+        self.encode_faces()
 
     def encode_faces(self):
         for image in os.listdir('DATA_SETS'):
@@ -38,7 +37,68 @@ class Facerecognition:
 
         print(self.known_face_names)
 
+    def run_recognition(self):
+       video_capture = cv2.VideoCapture(0) 
+
+       if not video_capture.isOpened():
+           sys.exit('Video source not found...')
+
+        while True:
+           ret, frame = video_capture.read()
+
+           if self.process_current_frame:
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy= 0.25)
+            rgb_small_frame = small_frame [:, :, ::-1]
+
+
+            self.face_locations = face_recognition.face_locations(rgb_small_frame)
+            self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+            
+            self.face_names = []
+            for face_encoding in self.face_encodings:
+                matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                name = 'Error'
+                confidence = 'Error'
+
+                face_distances = face_recognition_distance(self.known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+
+
+                if matches [best_match_index]:
+                    name = self.known_face_names[best_match_index]
+                    confidence = face_confidence(face_distances)
+
+
+                    self.face_names.append(f'{name}  ({confidence})')
+                
+                self.process_current_frame = not self.process_current_frame
+       
+            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                top*=4
+                right*=4
+                bottom*=4
+           left*=4
+
+           cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+           cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), -1)
+           cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8 (255,255,255),1)
+
+
+           cv2.imshow('Face Recognition', frame)
+
+           if cv2.waitKey(1) ==ord('q'):
+               break
+           
+        video_capture.release()
+       cv2.destroyAllWindows()
+
+       
+
+       
+
 if __name__ == '__main__':
-    fr = Facerecognition()
+    fr = FaceRecognition
+    fr.run_recognition()
+
 
 
